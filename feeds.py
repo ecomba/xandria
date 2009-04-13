a19877aa6a16fb8ca9be5821b20d7ad395637f72
Enrique Comba Riepenhausen <ecomba@nexwerk.com>
2009-04-13 18:56:02 +0100

    Multiple Feeds
    Now we can add more then one feed and return them

--- feeds.py
+++ feeds.py
@@ -1,2 +1,13 @@
-class Feed(object):
-  pass
+from google.appengine.ext import db
+
+class Feed(db.Model):
+  url = db.StringProperty()
+  
+  def save(self):
+    self.put()
+  
+  @staticmethod  
+  def findAll():
+    return Feed.all().fetch(limit = 1000)
+    
+    
--- test/test_feeds.py
+++ test/test_feeds.py
@@ -2,9 +2,27 @@ import unittest
 from feeds import Feed
 
 class FeedTest(unittest.TestCase):
-  def test_should_exist(self):
-    """Is the class there?"""
+  def setUp(self):
+    self.url = 'url'
+    self.feed = Feed(url = self.url)
+
+  def testShouldExist(self):
     try:
-      Feed()
+      Feed(url = None)
     except NameError, e:
       self.fail(str(e) + 'The Feeds class does not exist! What are you waiting for?')
+      
+  def testFeedShouldHaveUrl(self):
+    self.assertEquals(self.url, self.feed.url)
+    
+  def testShouldProvideEmptyListIfNoFeedsExist(self):
+    self.assertEquals([], Feed.findAll())
+    
+  def testShouldSaveAFeedAndReturnAListWithOneFeedOnly(self):
+    self.feed.save()
+    self.assertEquals(1, len(Feed.findAll()))
+  
+  def testShouldSaveMoreThanOneFeedAndReturnThem(self):
+    self.feed.save()
+    Feed(url='some.other.url').save()
+    self.assertEquals(2, len(Feed.findAll()))
