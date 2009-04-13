26775cf70d9d5cf33c0311f4a6c44d6ea1f3341d
Enrique Comba Riepenhausen <ecomba@nexwerk.com>
2009-04-13 19:46:47 +0100

    Phantom Menace
    Or how we didn't see that name is NOT name

--- feeds.py
+++ feeds.py
@@ -1,5 +1,25 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
+class FeedsPage(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.out.write('Welcome to Aggregator')
+
+    # feeds = Feed.findAll()
+    # for feed in feeds:
+    #   self.response.out.write(feed.url)
+    
+
+application = webapp.WSGIApplication([('/feeds', FeedsPage)], debug=True)
+
+def main():
+  run_wsgi_app(application)
+  
+if __name__ == "__main__":
+  main()
+  
 class Feed(db.Model):
   url = db.StringProperty()
   
